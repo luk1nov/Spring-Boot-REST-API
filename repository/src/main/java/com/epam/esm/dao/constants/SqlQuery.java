e833d4f1c093ec3fa8550f590f87000529739fe6
Anton <lukyanoff1337@gmail.com>
2022-08-09 23:00:30 +0300

    find all & by id queries added

--- repository/src/main/java/com/epam/esm/dao/constants/SqlQuery.java
+++ repository/src/main/java/com/epam/esm/dao/constants/SqlQuery.java
@@ -2,4 +2,6 @@ package com.epam.esm.dao.constants;
 
 public class SqlQuery {
     public static final String INSERT_TAG = "INSERT INTO tag (name) VALUES (?)";
+    public static final String FIND_ALL_TAGS = "SELECT id, name FROM tag ORDER BY id";
+    public static final String FIND_TAG_BY_ID = "SELECT id, name FROM tag WHERE id = ?";
 }
