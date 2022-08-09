9e5e50617b43868d2e2eb120443f48ae8a690381
Anton <lukyanoff1337@gmail.com>
2022-08-09 22:57:09 +0300

    renamed

--- web/src/main/java/com/epam/esm/config/WebConfig.java
+++ web/src/main/java/com/epam/esm/config/WebConfig.java
@@ -1,33 +1,29 @@
 package com.epam.esm.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @ComponentScan("com.epam.esm")
 @EnableWebMvc
-public class SpringConfig implements WebMvcConfigurer {
-    private final ApplicationContext context;
+public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    public SpringConfig(ApplicationContext context) {
-        this.context = context;
+    private static final String ERROR_MESSAGES_FILE = "/error_messages";
+    private static final String ENCODING = "UTF-8";
+
+    @Bean
+    public ResourceBundleMessageSource getResourceBundleMessageSource() {
+        ResourceBundleMessageSource messages = new ResourceBundleMessageSource();
+        messages.addBasenames(ERROR_MESSAGES_FILE);
+        messages.setDefaultEncoding(ENCODING);
+        return messages;
     }
 
-
-    @Override
+    /*@Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
                 .json()
@@ -35,5 +31,5 @@ public class SpringConfig implements WebMvcConfigurer {
                 .build()
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
-    }
+    }*/
 }
