4133825282f337af685ad24b0e618e4f74b8a1b6
Anton <lukyanoff1337@gmail.com>
2022-08-12 02:13:14 +0300

    disabled write dates as timestamps

--- web/src/main/java/com/epam/esm/config/WebConfig.java
+++ web/src/main/java/com/epam/esm/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.epam.esm.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan("com.epam.esm")
@@ -23,13 +30,11 @@ public class WebConfig implements WebMvcConfigurer {
         return messages;
     }
 
-    /*@Override
+    @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
-                .json()
-                .modules(new JavaTimeModule())
-                .build()
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
-    }*/
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        converters.add(new MappingJackson2HttpMessageConverter(mapper));
+    }
 }
