a9afa343587300dcc8c3edc356e63556a61f4a92
Anton Lukyanov <antoshal@gmail.com>
2022-08-17 18:41:23 +0300

    upd constructor

--- service/src/main/java/com/epam/esm/exceptions/InvalidDataProvidedException.java
+++ service/src/main/java/com/epam/esm/exceptions/InvalidDataProvidedException.java
@@ -3,6 +3,10 @@ package com.epam.esm.exceptions;
 public class InvalidDataProvidedException extends RuntimeException{
     private final int ERROR_CODE = 50001;
 
+    public InvalidDataProvidedException(String message) {
+        super(message);
+    }
+
     public int getErrorCode() {
         return ERROR_CODE;
     }
