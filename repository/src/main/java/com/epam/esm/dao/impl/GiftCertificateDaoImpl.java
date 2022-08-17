7527514a4541e624e4aceb5b79a68885fa883636
Anton Lukyanov <antoshal@gmail.com>
2022-08-17 18:34:14 +0300

    added new features

--- repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
+++ repository/src/main/java/com/epam/esm/dao/impl/GiftCertificateDaoImpl.java
@@ -3,7 +3,6 @@ package com.epam.esm.dao.impl;
 import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.dao.mapper.GiftCertificateExtractor;
-import com.epam.esm.dao.mapper.GiftCertificateRowMapper;
 import com.epam.esm.model.GiftCertificate;
 import com.epam.esm.model.Tag;
 import org.apache.logging.log4j.LogManager;
@@ -31,14 +30,12 @@ public class GiftCertificateDaoImpl implements GiftCertificateDao {
     private static final Logger logger = LogManager.getLogger();
     private final JdbcTemplate jdbcTemplate;
     private final TagDao tagDao;
-    private final GiftCertificateRowMapper rowMapper;
     private final GiftCertificateExtractor extractor;
 
     @Autowired
-    public GiftCertificateDaoImpl(JdbcTemplate jdbcTemplate, TagDao tagDao, GiftCertificateRowMapper rowMapper, GiftCertificateExtractor extractor) {
+    public GiftCertificateDaoImpl(JdbcTemplate jdbcTemplate, TagDao tagDao, GiftCertificateExtractor extractor) {
         this.jdbcTemplate = jdbcTemplate;
         this.tagDao = tagDao;
-        this.rowMapper = rowMapper;
         this.extractor = extractor;
     }
 
@@ -73,20 +70,39 @@ public class GiftCertificateDaoImpl implements GiftCertificateDao {
 
     @Override
     public int update(Long id, GiftCertificate giftCertificate) {
-        return 0;
+        return jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(UPDATE_GIFT_CERTIFICATE);
+            ps.setString(1, giftCertificate.getName());
+            ps.setString(2, giftCertificate.getDescription());
+            ps.setBigDecimal(3, giftCertificate.getPrice());
+            ps.setInt(4, giftCertificate.getDuration());
+            ps.setTimestamp(5, Timestamp.valueOf(giftCertificate.getLastUpdateDate()));
+            return ps;
+        });
     }
 
     @Override
     public int deleteById(Long id) {
-        return 0;
+        return jdbcTemplate.update(DELETE_CERTIFICATE_BY_ID, id);
     }
 
     @Override
     public Set<Tag> setTagsToCertificate(Long id, Set<Tag> addedTags) {
+        logger.info(addedTags);
         addedTags = addedTags.stream().map(tagDao::findOrCreate).collect(Collectors.toSet());
         for (Tag tag : addedTags){
-            jdbcTemplate.update(SET_TAG_FOR_CERTIFICATE, tag.getId(), id);
+            jdbcTemplate.update(SET_TAG_FOR_CERTIFICATE, id, tag.getId());
         }
         return addedTags;
+    }
+
+    @Override
+    public boolean removeAllTags(Long id) {
+        return jdbcTemplate.update(DELETE_ALL_TAGS_FROM_CERTIFICATE, id) > 0;
+    }
+
+    @Override
+    public List<GiftCertificate> findCertificatesByTagId(Long id) {
+        return jdbcTemplate.query(FIND_CERTIFICATE_BY_TAG, extractor, id);
     }
 }
