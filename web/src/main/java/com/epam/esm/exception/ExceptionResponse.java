7c107c9ed176f00eb33844efe3ac755d1ccc2e6b
Anton Lukyanov <antoshal@gmail.com>
2022-08-17 19:15:43 +0300

    set description as non required for response

--- web/src/main/java/com/epam/esm/exception/ExceptionResponse.java
+++ web/src/main/java/com/epam/esm/exception/ExceptionResponse.java
@@ -1,5 +1,6 @@
 package com.epam.esm.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionResponse {
     private int errorCode;
     private String message;
