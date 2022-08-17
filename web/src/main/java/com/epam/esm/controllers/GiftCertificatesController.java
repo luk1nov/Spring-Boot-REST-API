7049adf3701e7e94c205fb02481251cc22008340
Anton Lukyanov <antoshal@gmail.com>
2022-08-17 18:41:38 +0300

    finished mappings

--- web/src/main/java/com/epam/esm/controllers/GiftCertificatesController.java
+++ web/src/main/java/com/epam/esm/controllers/GiftCertificatesController.java
@@ -1,12 +1,14 @@
 package com.epam.esm.controllers;
 
 import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.TagDto;
 import com.epam.esm.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/certificates")
@@ -33,5 +35,30 @@ public class GiftCertificatesController {
     @GetMapping
     public List<GiftCertificateDto> findAll(){
         return service.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public GiftCertificateDto delete(@PathVariable Long id){
+        return service.deleteById(id);
+    }
+
+    @PutMapping("/add_tags/{id}")
+    public Set<TagDto> addTagsToCertificate(@PathVariable Long id, @RequestBody Set<TagDto> tags){
+        return service.addTags(id, tags);
+    }
+
+    @DeleteMapping("/delete_tags/{id}")
+    public GiftCertificateDto removeAllTags(@PathVariable Long id){
+        return service.removeALlTags(id);
+    }
+
+    @GetMapping("/search_by_tag/{id}")
+    public List<GiftCertificateDto> findCertificatesByTag(@PathVariable Long id){
+        return service.findCertificatesByTag(id);
+    }
+
+    @PatchMapping("/{id}")
+    public GiftCertificateDto update(@PathVariable Long id, @RequestBody GiftCertificateDto giftCertificateDto){
+        return service.update(id, giftCertificateDto);
     }
 }
