f758176af20dc0bf49787e77d7ebeb14609059e1
Anton <lukyanoff1337@gmail.com>
2022-08-26 01:04:05 +0300

    implemented upd services

--- web/src/main/java/com/epam/esm/controllers/TagsController.java
+++ web/src/main/java/com/epam/esm/controllers/TagsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tags")
+@RequestMapping("/api/tags")
 public class TagsController {
     private final TagService tagService;
 
