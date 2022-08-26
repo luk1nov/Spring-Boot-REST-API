86b18178fc96ee2b225db95759ba6cabc0f4ca79
Anton <lukyanoff1337@gmail.com>
2022-08-27 01:20:31 +0300

    added additional method to TagRepository

--- repository/src/main/java/com/epam/esm/repository/TagRepository.java
+++ repository/src/main/java/com/epam/esm/repository/TagRepository.java
@@ -3,5 +3,8 @@ package com.epam.esm.repository;
 import com.epam.esm.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
 }
