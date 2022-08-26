fa4f01e36e27eb4e4a50a2580ca57c7683e17397
Anton <lukyanoff1337@gmail.com>
2022-08-27 01:21:50 +0300

    refactor

--- service/src/main/java/com/epam/esm/dto/TagDto.java
+++ service/src/main/java/com/epam/esm/dto/TagDto.java
@@ -1,40 +1,20 @@
 package com.epam.esm.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
-@EqualsAndHashCode
-@ToString
+@Data
 @Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class TagDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private long id;
 
     private String name;
 
-    public TagDto() {
-    }
-
-    public TagDto(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public TagDto(String name) {
         this.name = name;
     }
 }
