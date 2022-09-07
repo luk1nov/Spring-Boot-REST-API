b7cc98d707a3c105e515e8dc4306a650b7e0183d
Anton Lukyanov <antoshal@gmail.com>
2022-09-08 00:10:05 +0300

    added builder and prePersist method

--- repository/src/main/java/com/epam/esm/model/Order.java
+++ repository/src/main/java/com/epam/esm/model/Order.java
@@ -1,6 +1,6 @@
 package com.epam.esm.model;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -9,6 +9,9 @@ import java.util.List;
 
 @Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "orders")
 public class Order {
     @Id
@@ -24,9 +27,17 @@ public class Order {
     @JoinColumn(name = "users_id", nullable = false)
     private User user;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @ManyToMany
     @JoinTable(name = "order_has_gift_certificate",
             joinColumns = @JoinColumn(name = "orders_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "gift_certificate_id", referencedColumnName = "id"))
     private List<GiftCertificate> giftCertificateList;
+
+    @PrePersist
+    public void prePersist(){
+        orderDate = LocalDateTime.now();
+        cost = giftCertificateList.stream().map(GiftCertificate::getPrice).reduce(BigDecimal.ONE, BigDecimal::add);
+    }
 }
