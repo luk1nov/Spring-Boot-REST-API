cf3c46e6a1d0bdc4375e50966f943599ed2c4c22
Anton Lukyanov <antoshal@gmail.com>
2022-09-13 01:10:18 +0300

    added search by tag

--- repository/src/main/java/com/epam/esm/repository/GiftCertificateRepository.java
+++ repository/src/main/java/com/epam/esm/repository/GiftCertificateRepository.java
@@ -1,7 +1,11 @@
 package com.epam.esm.repository;
 
 import com.epam.esm.model.GiftCertificate;
+import com.epam.esm.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GiftCertificateRepository extends JpaRepository<GiftCertificate, Long> {
+    List<GiftCertificate> findGiftCertificatesByTagListIsContaining(Tag tag);
 }
