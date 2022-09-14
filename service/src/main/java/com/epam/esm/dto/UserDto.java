13c2de6209be8470f0dd3cdf86605b94a3c48a8e
Anton Lukyanov <antoshal@gmail.com>
2022-09-14 21:58:30 +0300

    added hateoas support

--- service/src/main/java/com/epam/esm/dto/UserDto.java
+++ service/src/main/java/com/epam/esm/dto/UserDto.java
@@ -1,10 +1,17 @@
 package com.epam.esm.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class UserDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto extends RepresentationModel<UserDto> {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private long id;
     private String firstName;
